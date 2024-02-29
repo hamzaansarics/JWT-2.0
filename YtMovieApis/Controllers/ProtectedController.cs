2b9844e836f5f1f17bc0e09098e3867331857554
Hamza Ansari <hamzaansarics@gmail.com>
2024-02-29 14:33:30 +0500

    Add project files.

---
+++ YtMovieApis/Controllers/ProtectedController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YtMovieApis.Models.DTO;
+
+namespace YtMovieApis.Controllers
+{
+    [Route("api/[controller]/{action}")]
+    [ApiController]
+    [Authorize(Roles ="User")]
+    public class ProtectedController : ControllerBase
+    {
+        public IActionResult GetData()
+        {
+            var status = new Status();
+            status.StatusCode = 1;
+            status.Message = "Data from protected controller";
+            return Ok(status);
+        }
+    }
+}
